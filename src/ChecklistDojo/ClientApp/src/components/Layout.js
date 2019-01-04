eaf883d4f546c1be2b6ad621e9d1e82deb72c3bb
eschoenborn <eschoenborn@magicwriter.com>
2019-01-04 10:47:14 -0500

    Revert "Update Layout.js"
    This reverts commit 89c61e833a8499eb7ceaf214c673767e4d4a5909.
    
    # Conflicts:
    #	src/ChecklistDojo/ClientApp/src/components/Layout.js

--- src/ChecklistDojo/ClientApp/src/components/Layout.js
+++ src/ChecklistDojo/ClientApp/src/components/Layout.js
@@ -8,8 +8,8 @@ export class Layout extends Component {
     render() {
         return (
             <div>
-            <NavMenu />
-            <Container>{this.props.children}</Container>
+            	<NavMenu />
+            	<Container>{this.props.children}</Container>
             </div>
         );
     }
