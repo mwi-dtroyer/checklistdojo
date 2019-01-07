73c5c155449d5977d817f095faee7556fcbb1d97
dtroyer <dtroyer@magicwrighter.com>
2019-01-07 14:11:25 -0500

    GH-2 - Use localStorage to show whether or not a user is logged in

--- src/ChecklistDojo/ClientApp/src/components/NavMenu.js
+++ src/ChecklistDojo/ClientApp/src/components/NavMenu.js
@@ -30,53 +30,84 @@ export class NavMenu extends Component {
   }
 
   render() {
-    return (
-      <header>
-        <Navbar
-          className="navbar-expand-md navbar-toggleable-md ng-white border-bottom box-shadow mb-3"
-          light
-        >
-          <Container>
-            <NavbarBrand tag={Link} to="/">
-              Checklist Dojo
-            </NavbarBrand>
-            <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-            <Collapse
-              className="d-md-inline-flex flex-md-row-reverse"
-              isOpen={!this.state.collapsed}
-              navbar
-            >
-              <ul className="navbar-nav flex-grow">
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/">
-                    Dashboard
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/templates">
-                    Templates
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/scheduled">
-                    Scheduled
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/history">
-                    History
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/settings">
-                    Settings
-                  </NavLink>
-                </NavItem>
-              </ul>
-            </Collapse>
-          </Container>
-        </Navbar>
-      </header>
-    );
+    if (localStorage.getItem("isLoggedIn", "true")) {
+      return (
+        <header>
+          <Navbar
+            className="navbar-expand-md navbar-toggleable-md ng-white border-bottom box-shadow mb-3"
+            light
+          >
+            <Container>
+              <NavbarBrand tag={Link} to="/">
+                Checklist Dojo
+              </NavbarBrand>
+              <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+              <Collapse
+                className="d-md-inline-flex flex-md-row-reverse"
+                isOpen={!this.state.collapsed}
+                navbar
+              >
+                <ul className="navbar-nav flex-grow">
+                  <NavItem>
+                    <NavLink tag={Link} className="text-dark" to="/">
+                      Dashboard
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink tag={Link} className="text-dark" to="/templates">
+                      Templates
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink tag={Link} className="text-dark" to="/scheduled">
+                      Scheduled
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink tag={Link} className="text-dark" to="/history">
+                      History
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink tag={Link} className="text-dark" to="/settings">
+                      Settings
+                    </NavLink>
+                  </NavItem>
+                </ul>
+              </Collapse>
+            </Container>
+          </Navbar>
+        </header>
+      );
+    } else {
+      return (
+        <header>
+          <Navbar
+            className="navbar-expand-md navbar-toggleable-md ng-white border-bottom box-shadow mb-3"
+            light
+          >
+            <Container>
+              <NavbarBrand tag={Link} to="/">
+                Checklist Dojo
+              </NavbarBrand>
+              <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+              <Collapse
+                className="d-md-inline-flex flex-md-row-reverse"
+                isOpen={!this.state.collapsed}
+                navbar
+              >
+                <ul className="navbar-nav flex-grow">
+                  <NavItem>
+                    <NavLink tag={Link} className="text-dark" to="/">
+                      Dashboard
+                    </NavLink>
+                  </NavItem>
+                </ul>
+              </Collapse>
+            </Container>
+          </Navbar>
+        </header>
+      );
+    }
   }
 }
